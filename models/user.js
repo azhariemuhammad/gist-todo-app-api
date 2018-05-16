6ec1d8b1566f6f2f09d4766a61dec23e67662af5
azhariemuhammad <azharie_muhammad@live.com>
2018-05-17 06:08:24 +0700

    create new migration to add UserId on task table

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   User.associate = function(models) {
     // associations can be defined here
+    User.hasMany(models.Task, { foreignKey: "UserId" });
   };
   return User;
 };
